babe8347f177d4269a80ceb28e3676f44a906625
sharatkhanna <36418526+sharatkhanna@users.noreply.github.com>
2018-02-17 15:48:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-    res.sendFile('Article one requested and will be served');
+    res.send('Article one requested and will be served');
 });
 
 app.get('/article-two', function (req, res) {
-    res.sendFile('Article two requested and will be served');
+    res.send('Article two requested and will be served');
 });
 
 app.get('/article-three', function (req, res) {
-    res.sendFile('Article three requested and will be served');
+    res.send('Article three requested and will be served');
 });
 
 app.get('/ui/style.css', function (req, res) {
