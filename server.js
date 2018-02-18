5b56dcf643a99c24edeaa69676b152569b5a927a
sharatkhanna <36418526+sharatkhanna@users.noreply.github.com>
2018-02-18 21:58:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,86 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: 'Article One | Sharat Khanna',
+    heading: 'Article One',
+    date: '17 Feb, 2018',
+    content: `
+    <html>
+    <head>
+        <title>
+        Article One: Sharat Khanna    
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr/>
+            <h3>
+                Article One
+            </h3>
+            <div>
+                17 Feb, 2018
+            </div>
+            <div>
+                <p>
+                    This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+                </p>
+                <p>
+                    This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+                </p>
+                <p>
+                    This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+                </p>
+            </div>
+        </div>
+    </body>
+</html>
+`
+        
+        };
+        
+function createTemplate(data) {
+    var title = data.title;
+    var date =  data.date;
+    var heading = data.heading;
+    var content =  data.content;
+
+
+var htmlTemplate = `
+        <html>
+            <head>
+                <title>
+                Article One: Sharat Khanna    
+                </title>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link href="/ui/style.css" rel="stylesheet" />
+            </head>
+            <body>
+                <div class="container">
+                    <div>
+                        <a href="/">Home</a>
+                    </div>
+                    <hr/>
+                    <h3>
+                        ${heading}
+                    </h3>
+                    <div>
+                        ${date}
+                    </div>
+                    <div>
+                        ${content}
+                    </div>
+                </div>
+            </body>
+        </html>`;
+        return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
