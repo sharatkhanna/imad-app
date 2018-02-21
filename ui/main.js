7888089636e965f60bfbc9d5cf6ca790f825e2f8
sharatkhanna <36418526+sharatkhanna@users.noreply.github.com>
2018-02-21 22:35:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,11 @@ var element = document.getElementById('main-text');
 element.innerHTML = "New Value";
 
 var img = document.getElementById('madi');
-madi.onclick = function () {
-    madi.style.marginLeft = '100px';  
+var marginLeft = 0;
+var moveRight = function() {
+  marginLeft += 10;
+  img.style.marginLeft = marginLeft + 'px';
+};
+    madi.onclick = function () {
+    var interval = setInterval(moveRight, 100);  
 };
