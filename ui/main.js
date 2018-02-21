a46a9b2d614f056db4db6b8748cf55648ce09aaf
sharatkhanna <36418526+sharatkhanna@users.noreply.github.com>
2018-02-21 22:25:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,8 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = "New Value";
+
+var img = document.getElementById('madi');
+madi.onclick = function () {
+    madi.style.marginLeft = 100px;  
+};
