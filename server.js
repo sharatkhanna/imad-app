03c1da0834f2e6533d9b8a013b5e18f20d06cc3b
sharatkhanna <36418526+sharatkhanna@users.noreply.github.com>
2018-02-21 23:23:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,11 +98,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName', function (req, res) {
+//app.get('/:articleName', function (req, res) {
     
-    res.send(createTemplate(articles[articleName]));
+//    res.send(createTemplate(articles[articleName]));
     
-});
+//});
 
 var counter = 0;
 app.get('/counter', function (req, res) {
