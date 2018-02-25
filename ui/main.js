a60bcaf6650d0f33c4649c99747a4afe509303ba
sharatkhanna <36418526+sharatkhanna@users.noreply.github.com>
2018-02-25 20:53:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,8 @@
 var button = document.getElementById('counter');
 var counter = 0;
 button.onClick = function () {
-  counter = counter + 1;
-  var span = document.getElementById('count');
+  
+  //counter = counter + 1;
+  //var span = document.getElementById('count');
   span.innerHTML = 20;
 };
