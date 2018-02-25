7527e325d602941ba060b7330e051df5a1c53ea2
sharatkhanna <36418526+sharatkhanna@users.noreply.github.com>
2018-02-25 20:43:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,9 @@
-console.log('Loaded!');
-//change the text contents
+//counter function
 
-var element = document.getElementById('main-text');
-
-element.innerHTML = "New Value";
-
-var img = document.getElementById('madi');
-var marginLeft = 0;
-var moveRight = function() {
-  marginLeft += 1;
-  img.style.marginLeft = marginLeft + 'px';
-};
-    madi.onclick = function () {
-    var interval = setInterval(moveRight, 50);  
+var button = document.getElementById('counter');
+var counter = 0;
+button.onClick = function () {
+  counter += 1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
 };
